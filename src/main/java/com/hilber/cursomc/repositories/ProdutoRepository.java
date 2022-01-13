e20fdd1720accafd5e856bb28671980b33746ddb
unknown <hylber.kailton2014@gmail.com>
2022-01-13 15:08:51 -0300

    Produto e associacao muito para muitos

---
+++ src/main/java/com/hilber/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,27 @@
+package com.hilber.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hilber.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+
+
+	
+
+	
+
+
+
+	
+
+	
+
+	
+
+	
+
+}
