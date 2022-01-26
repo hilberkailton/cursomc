6bb7c162635100b8c26eedd2fa95077e210d165f
unknown <hylber.kailton2014@gmail.com>
2022-01-26 10:49:31 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/hilber/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,27 @@
+package com.hilber.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hilber.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+
+
+	
+
+	
+
+
+
+	
+
+	
+
+	
+
+	
+
+}
